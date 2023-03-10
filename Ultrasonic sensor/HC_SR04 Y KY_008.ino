907257c532e7a76509253e2ab5c6f46319efcb5b
JorgeADnro <106614143+JorgeADnro@users.noreply.github.com>
2023-03-10 13:21:53 -0600

    Add files via upload

---
+++ Ultrasonic sensor/HC_SR04 Y KY_008.ino
@@ -0,0 +1,45 @@
+const int trigPin = 4;
+const int echoPin = 2;
+int laserPin=21;
+
+//define sound speed in cm/uS
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+
+long duration;
+float distanceCm;
+float distanceInch;
+
+void setup() {
+  Serial.begin(115200); // Starts the serial communication
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(laserPin,OUTPUT);
+}
+
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculate the distance
+  distanceCm = duration * SOUND_SPEED/2;
+  
+  
+  // Prints the distance in the Serial Monitor
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+  delay(1000);
+  
+  if(distanceCm<10){
+    digitalWrite(laserPin,HIGH);
+    delay(1000);
+    digitalWrite(laserPin,LOW);
+     }
+}
